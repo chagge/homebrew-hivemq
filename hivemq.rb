fb4c4f02665a114e237c5a4bd5b0a48b2ee5aba0
Obermaier Dominik <dominik.obermaier@gmail.com>
2013-10-13 21:04:29 +0200

    Added HiveMQ Formula

---
+++ hivemq.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Hivemq < Formula
+  homepage 'http://www.hivemq.com'
+  url 'http://www.hivemq.com/wp-content/uploads/hivemq-1.4.1.zip'
+  sha1 'd809e7d3b35a0626665e37fd70fc4684faff419f'
+
+  def install
+    cp 'bin/run.sh', 'bin/hivemq'
+    libexec.install Dir['*']
+    bin.write_exec_script libexec/'bin/hivemq'
+  end
+
+  def caveats
+    "To configure HiveMQ, execute 'vi " + libexec + "/conf/configuration.properties'"
+  end
+end
