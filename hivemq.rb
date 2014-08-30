ca4b671ed18c27db6f0b9ce6ca96cb4dd1bb3552
Obermaier Dominik <dominik.obermaier@gmail.com>
2014-08-30 20:45:14 +0200

    Update hivemq.rb

--- hivemq.rb
+++ hivemq.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Hivemq < Formula
   homepage 'http://www.hivemq.com'
-  url 'http://www.hivemq.com/wp-content/uploads/hivemq-2.0.1.zip'
-  sha1 '2888c213c73ec5a923dd63a3e1d3f18a87faae48'
+  url 'http://www.hivemq.com/wp-content/uploads/hivemq-2.0.2.zip'
+  sha1 '20ece51f558c1dd7b7c8147cc9e7f24d1da22e71'
 
   def install
     cp 'bin/run.sh', 'bin/hivemq'
